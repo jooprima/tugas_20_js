22f2f5a46cb6b47f8501ec80bc3d1bd3b8d94ddc
yohanes_prima <jooprima@gmail.com>
2019-07-14 23:21:47 +0700

    Tugas 20 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas20.js
@@ -0,0 +1,9 @@
+function myMatch() {
+  var kalimat =
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+  console.log(kalimat.match("are"));
+  console.log(kalimat.match("words"));
+}
+
+myMatch();
